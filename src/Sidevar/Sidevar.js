c433f5dcd68e0772deed9988014d62dca9b33e75
hp <niranjan962796@gmail.com>
2024-09-02 17:05:31 +0530

    second commit

--- src/Sidevar/Sidevar.js
+++ src/Sidevar/Sidevar.js
@@ -1,4 +1,10 @@
+import { FaMessage } from "react-icons/fa6";
+import { FaSave } from "react-icons/fa";
 import "./Sidevar.css";
+import { MdGroup } from "react-icons/md";
+import { MdEmojiEvents } from "react-icons/md";
+
+
 
 
 
@@ -6,6 +12,7 @@ import "./Sidevar.css";
 
 const Sidevar=()=>{
     return (
+        <>
         <div className="sidevar-1">
             <div className="side-img">
                 <img className="side-img-1" src="https://media.licdn.com/dms/image/v2/D4D16AQGRMPaWW9N8AA/profile-displaybackgroundimage-shrink_350_1400/profile-displaybackgroundimage-shrink_350_1400/0/1674849632924?e=1730937600&v=beta&t=zllPk9EbBJyR5wQwD6IQBJYFySuvebe1y7BJ5LFmGko"></img>
@@ -17,6 +24,19 @@ const Sidevar=()=>{
             <h5>Full Stack Developer</h5>
             <p>Delhi Indhira Vihar 63278</p>
         </div>
+
+        <div className="sidevar-2">
+<p>Accelerate your career with</p>
+<p>Premioum</p>
+<h4><FaMessage /> Try For Rs.0</h4>
+        </div>
+
+        <div className="sidevar-3">
+            <span> <FaSave />Save Item</span>
+            <span><MdGroup /> Groups</span>
+            <span><MdEmojiEvents /> Event</span>
+        </div>
+        </>
     )
 }
 export default Sidevar;
